7d88453f1aa34ab3d3e2e8e3287337df541c573b
Carolina Amadio <33064803+CarolinaAmadio@users.noreply.github.com>
2023-05-23 10:46:58 +0000

    Create area_plot.py
    visualisation from timelist

---
+++ VISUALIZATION/area_plot.py
@@ -0,0 +1,9 @@
+# export ONLINE_REPO=/g100_scratch/userexternal/camadio0/Multivariate_Assimilation_2017_2018/ONLINE/
+# bitsea imported 
+
+""" plot of a time series (barplot - area ...)
+input: dataframe 
+output: fig.png 
+"""
+
+#/g100/home/userexternal/camadio0/Time_Series_plot/NITRATE_AVAILABILITY_2015_2020_V9C
