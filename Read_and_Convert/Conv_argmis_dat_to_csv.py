7c54d37863c7da2ec3c8d60c785118f8ed508fe8
Carolina Amadio <33064803+CarolinaAmadio@users.noreply.github.com>
2023-07-03 12:48:34 +0200

    Update Conv_argmis_dat_to_csv.py
    insert comment to recap vars code in VAR_type column

--- Read_and_Convert/Conv_argmis_dat_to_csv.py
+++ Read_and_Convert/Conv_argmis_dat_to_csv.py
@@ -4,6 +4,11 @@ import pandas as pd
 INPUT='/g100_scratch/userexternal/camadio0/SYNTHETIC_2017_2018/wrkdir/MODEL/DA__FREQ_1/20181222.arg_mis.dat'
 df = pd.read_csv(INPUT, header=1, sep='\t', index_col=0)
 COL= ['Nr','VAR_type', 'lon', 'lat' , 'depth' ,'nr1','misfit', 'error', 'wmo']
+
+# VAR_type = 0 Chla
+# VAR_type = 1 N3n
+# VAR_type = 2 O2o
+
 df.index =np.arange(1,len(df)+1)
 s = pd.Series(df.columns)
 s.name= 0
