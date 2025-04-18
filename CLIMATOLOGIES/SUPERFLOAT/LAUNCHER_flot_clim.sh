942df6c911410758941351b8e2347ce379c28b7a
CarolinaAmadio <caio.spoleto@gmail.com>
2025-04-18 19:05:02 +0200

    Add LAUNCHER_flot_clim.sh script for climatology generation from BGC-Argo floats dataset

---
+++ CLIMATOLOGIES/SUPERFLOAT/LAUNCHER_flot_clim.sh
@@ -0,0 +1,41 @@
+#! /bin/bash
+
+#SBATCH --job-name=floatClim
+#SBATCH -N1
+#SBATCH --ntasks-per-node=16
+#SBATCH --time=00:20:00
+#SBATCH --mem=300gb
+#SBATCH --account=OGS23_PRACE_IT
+#SBATCH --partition=g100_usr_prod
+
+
+module load autoload
+module load intelmpi/oneapi-2021--binary
+module load intelmpi/oneapi-2021--binary netcdf/4.7.4--oneapi--2021.2.0-ifort ncview
+LIBDIR=/g100_work/OGS23_PRACE_IT/COPERNICUS/V10C/HOST/g100
+export    HDF5_DIR=$LIBDIR
+export NETCDF4_DIR=$LIBDIR
+export    GEOS_DIR=$LIBDIR
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$LIBDIR
+
+cd $SLURM_SUBMIT_DIR
+
+. ../profile.inc
+
+source /g100_work/OGS23_PRACE_IT/COPERNICUS/py_env_3.9.18_new/bin/activate
+export ONLINE_REPO=/g100/home/userexternal/camadio0/CLIMATOLOGIE/FLOAT_CLIMATOLOGIES/new_clim_FLOAT_V12C_2012_2025/
+
+
+export PYTHONPATH=$PYTHONPATH:/g100/home/userexternal/camadio0/CLIMATOLOGIE/FLOAT_CLIMATOLOGIES/new_clim_FLOAT_V12C_2012_2025/bit.sea/src/
+
+VARNAME='O2o'
+OUTDIR=/g100/home/userexternal/camadio0/CLIMATOLOGIE/FLOAT_CLIMATOLOGIES/new_clim_FLOAT_V12C_2012_2025/
+
+mkdir -p $OUTDIR
+
+
+my_prex "python 0_count_avai_nprofiles.py -o $OUTDIR -v $VARNAME"
+my_prex "python Yr_Climfloat_netcdf.py -o $OUTDIR -v $VARNAME"
+mkdir -p FIGURE
+mv *png FIGURE/
+python compare_clima.py
